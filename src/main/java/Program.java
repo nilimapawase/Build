45932677da14b74e1496c9ca2644285dcd7bef88
nilimapawase <nilimapawase14@gmail.com>
2026-05-20 17:34:11 +0530

    Initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,24 @@
+
+public class Program {
+
+	public void GrestestNumber()
+	{
+		int []a= {20,50,60,70,100,250,150};
+		int max=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]>max)
+			{
+				max=a[i];
+			}
+		}
+		System.out.println("max number="+max);
+	}
+	public static void main(String[] args) {
+		
+		Program p=new Program();
+		p.GrestestNumber();
+
+	}
+
+}
